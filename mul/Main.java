f1f66c99aa5d99ad7ae507ceeab6918257137d6f
Wooki <hyunook02@naver.com>
2024-03-31 20:03:34 +0900

    boj 10998: AxB

---
+++ mul/Main.java
@@ -0,0 +1,10 @@
+package Backjoon.mul;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(in.nextInt() * in.nextInt());
+    }
+}
