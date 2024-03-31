bc0cb2ccdd09e643c42c270d0194d32e96def967
Wooki <hyunook02@naver.com>
2024-03-31 20:26:54 +0900

    boj 11382: 꼬마 정민

---
+++ add/littleJungmin/Main.java
@@ -0,0 +1,10 @@
+package Backjoon.add.littleJungmin;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(in.nextLong() + in.nextLong() + in.nextLong());
+    }
+}
