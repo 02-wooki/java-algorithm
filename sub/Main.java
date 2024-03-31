4fd8c50f6703eaf227e2b98452f02dad68ba42d3
Wooki <hyunook02@naver.com>
2024-03-31 20:00:12 +0900

    boj 1001: A-B

---
+++ sub/Main.java
@@ -0,0 +1,13 @@
+package Backjoon.sub;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println(in.nextInt()-in.nextInt());
+
+    }
+}
