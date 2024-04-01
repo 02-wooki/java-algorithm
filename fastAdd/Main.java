40e3536fdb79e4f2ce4f8b3bdc4a72757a350249
Wooki <hyunook02@naver.com>
2024-04-01 12:16:23 +0900

    boj 15552: 빠른 A+B

---
+++ fastAdd/Main.java
@@ -0,0 +1,25 @@
+package Backjoon.fastAdd;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+
+            String s = br.readLine();
+            StringTokenizer st = new StringTokenizer(s);
+
+            bw.write(String.format("%d\n", Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())));
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
