b1db230d7bc07cfe60364c80da658a2fd5ad1959
Wooki <hyunook02@naver.com>
2024-04-01 10:45:49 +0900

    boj 2884: 알람 시계

---
+++ alarm/Main.java
@@ -0,0 +1,20 @@
+package Backjoon.alarm;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int hour = in.nextInt();
+        int minute = in.nextInt();
+
+        if (minute < 45) {
+            hour--;
+            minute += 60;
+        }
+        if (hour < 0)
+            hour += 24;
+
+        System.out.printf("%d %d", hour, minute - 45);
+    }
+}
