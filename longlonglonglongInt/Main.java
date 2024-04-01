a1ec8da84ba73a4e324d5129b653d096e64dfe83
Wooki <hyunook02@naver.com>
2024-04-01 11:34:24 +0900

    boj 25314: 코딩은 체육과목 입니다

---
+++ longlonglonglongInt/Main.java
@@ -0,0 +1,16 @@
+package Backjoon.longlonglonglongInt;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String output = "";
+        for (int i = 0; i < n; i += 4) {
+            output += "long ";
+        }
+        System.out.println(output + "int");
+    }
+}
