24dab75c0f282372da2fd2fd9d7c1905d9f9ea54
Wooki <hyunook02@naver.com>
2024-04-04 10:08:12 +0900

    boj 2439: 별 찍기 - 2

---
+++ star2/Main.java
@@ -0,0 +1,20 @@
+package Backjoon.star2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < i-1; j++) {
+                System.out.printf(" ");
+            }
+            for (int j = i; j <= n; j++) {
+                System.out.printf("*");
+            }
+            System.out.println();
+        }
+    }
+}
