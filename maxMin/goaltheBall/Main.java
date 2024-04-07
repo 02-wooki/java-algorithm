e7f3ed6dc2aafd48a620da4aeb57b0f181c803e0
Wooki <hyunook02@naver.com>
2024-04-07 17:25:51 +0900

    boj 10810: 공 넣기

---
+++ maxMin/goaltheBall/Main.java
@@ -0,0 +1,34 @@
+package Backjoon.maxMin.goaltheBall;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int baguniLen = in.nextInt();
+        int[] baguni = new int[baguniLen];
+
+        for (int i = 0; i < baguni.length; i++) {
+            baguni[i] = 0;
+        }
+
+        int m = in.nextInt();
+        for (int i = 0; i < m; i++) {
+            int start = in.nextInt() - 1;
+            int end = in.nextInt() - 1;
+            int k = in.nextInt();
+
+            for (int j = start; j <= end; j++) {
+                baguni[j] = k;
+            }
+        }
+
+        for (int i :baguni) {
+            System.out.printf("%d ", i);
+        }
+
+
+    }
+}
