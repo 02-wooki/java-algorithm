0363a2fcf7c632a7d7319eb8de631f59a4cce06c
Wooki <hyunook02@naver.com>
2024-04-07 18:21:56 +0900

    boj 10811: 바구니 뒤집기

---
+++ reverseBaguni/Main.java
@@ -0,0 +1,37 @@
+package Backjoon.reverseBaguni;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] baguni = new int[n + 1];
+
+        for (int i = 0; i < baguni.length; i++) {
+            baguni[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            reverseArr(baguni, in.nextInt(), in.nextInt());
+        }
+
+        for (int i = 1; i < baguni.length; i++) {
+            System.out.printf("%d ", baguni[i]);
+        }
+
+    }
+
+    public static void reverseArr(int[] arr, int start, int end) {
+
+        for (int i = start; i <= (start + end) / 2; i++) {
+            int tmp = arr[i];
+            arr[i] = arr[start + (end - i)];
+            arr[start + (end - i)] = tmp;
+        }
+
+    }
+}
