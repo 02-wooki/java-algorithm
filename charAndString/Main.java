6ff3fdba7e65606fffde8c4575f6cfac22d0f9f4
Wooki <hyunook02@naver.com>
2024-04-08 11:00:44 +0900

    boj 27866: 문자와 문자열

---
+++ charAndString/Main.java
@@ -0,0 +1,13 @@
+package Backjoon.charAndString;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        char[] s = in.next().toCharArray();
+
+        System.out.println(s[in.nextInt() -1]);
+
+    }
+}
