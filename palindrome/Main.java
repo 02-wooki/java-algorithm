92c9d8aca70cafe84a56a27aa7b9bd82e80652dc
Wooki <hyunook02@naver.com>
2024-04-13 19:16:55 +0900

    boj 10988: 팰린드롬인지 확인하기

---
+++ palindrome/Main.java
@@ -0,0 +1,19 @@
+package Backjoon.palindrome;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        char[] c = in.next().toCharArray();
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < c.length / 2; i++) {
+            if (c[i] != c[c.length - 1 - i])
+                isPalindrome = false;
+        }
+
+        System.out.println(isPalindrome ? 1 : 0);
+    }
+}
