efd20d9280323ebe09d890481979fb2d7af58fd2
Wooki <hyunook02@naver.com>
2024-04-13 19:01:19 +0900

    boj 3003: 킹, 퀸, 룩, 비숍, 나이트, 폰

---
+++ Chesspieces/Main.java
@@ -0,0 +1,13 @@
+package Backjoon.Chesspieces;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.printf("%d %d %d %d %d %d",
+                1 - in.nextInt(), 1 - in.nextInt(), 2 - in.nextInt(), 2 - in.nextInt(), 2 - in.nextInt(), 8 - in.nextInt());
+
+    }
+}
