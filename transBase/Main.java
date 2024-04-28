8bcf93d8f297d878138bfc239d0cb73e1a88c37a
Wooki <hyunook02@naver.com>
2024-04-28 19:35:38 +0900

    boj 2745: 진법 변환

---
+++ transBase/Main.java
@@ -0,0 +1,35 @@
+package Backjoon.transBase;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        int base = in.nextInt();
+        int[] origin = strToInt(s);
+        int ten = 0, weight = 1;
+
+        for (int i = origin.length - 1; i >= 0; i--) {
+            ten += weight * origin[i];
+            weight *= base;
+        }
+
+        System.out.println(ten);
+    }
+
+    static int[] strToInt(String s) {
+        int[] i = new int[s.length()];
+
+        for (int j = 0; j < s.length(); j++) {
+            char c = s.charAt(j);
+            if (c >= '0' && c <= '9')
+                i[j] = c - '0';
+            else
+                i[j] = 10 + (c - 'A');
+        }
+
+        return i;
+    }
+}
