b367515ec1371255c63c367ad447e2f75a01cb73
Wooki <hyunook02@naver.com>
2024-04-28 20:16:56 +0900

    boj 2720: 세탁소 사장 동혁

--- laundryCEO/Main.java
+++ laundryCEO/Main.java
@@ -1,2 +1,31 @@
-package Backjoon.laundryCEO;public class Main {
+package Backjoon.laundryCEO;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] change = {25, 10, 5, 1};
+
+        for (int i = 0; i < n; i++) {
+            int money = in.nextInt();
+            int[] thistimeChange = new int[4];
+            int changeIndex = 0;
+            while (money > 0) {
+                if (change[changeIndex] > money)
+                    changeIndex++;
+                else {
+                    money -= change[changeIndex];
+                    thistimeChange[changeIndex]++;
+                }
+            }
+
+            for (int i1 : thistimeChange)
+                System.out.printf("%d ", i1);
+            System.out.println();
+        }
+
+    }
 }
