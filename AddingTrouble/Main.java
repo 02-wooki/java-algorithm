6c10bfce6bbc7898084cef97298b1f5b58be0fa2
Wooki <hyunook02@naver.com>
2024-08-02 12:49:32 +0900

    boj 31654: Adding Truble

---
+++ AddingTrouble/Main.java
@@ -0,0 +1,13 @@
+package Backjoon.AddingTrouble;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        if (in.nextInt() + in.nextInt() == in.nextInt())
+            System.out.println("correct!");
+        else
+            System.out.println("wrong!");
+    }
+}
