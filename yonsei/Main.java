a908f51ee4f29ce57347627a56542a7076138fb8
Wooki <hyunook02@naver.com>
2024-08-02 12:35:46 +0900

    boj 15680: 연세대학교

--- yonsei/Main.java
+++ yonsei/Main.java
@@ -1,2 +1,14 @@
-package Backjoon.yonsei;public class Main {
+package Backjoon.yonsei;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        if (in.nextInt() == 1) {
+            System.out.println("Leading the Way to the Future");
+        } else {
+            System.out.println("YONSEI");
+        }
+    }
 }
