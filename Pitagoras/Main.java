d46d027878b2a394ad02848955eab85430175541
Wooki <hyunook02@naver.com>
2024-08-02 15:27:27 +0900

    boj 4153: 직각삼각형

--- Pitagoras/Main.java
+++ Pitagoras/Main.java
@@ -5,10 +5,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt(), b = in.nextInt(), c = in.nextInt();
-        if (a * a + b * b == c * c)
-            System.out.println("right");
-        else
-            System.out.println("wrong");
+        while (in.hasNextLine()) {
+            int[] len = new int[3];
+            for (int i = 0; i < 3; i++)
+                len[i] = in.nextInt();
+
+            if (len[0] + len[1] + len[2] == 0)
+                break;
+
+            if (isTriangle(sort(len)))
+                System.out.println("right");
+            else
+                System.out.println("wrong");
+        }
+    }
+
+    public static boolean isTriangle(int[] a) {
+        return a[0]*a[0] + a[1]*a[1] == a[2]*a[2];
+    }
+
+    public static int[] sort(int[] arr) {
+        if (arr[0] > arr[1] && arr[0] > arr[2]) {
+            int tmp = arr[0];
+            arr[0] = arr[2];
+            arr[2] = tmp;
+        } else if (arr[1] > arr[0] && arr[1] > arr[2]) {
+            int tmp = arr[1];
+            arr[1] = arr[2];
+            arr[2] = tmp;
+        }
+
+        return arr;
     }
 }
