5422631d5961a619455a0318d202a220182ebd46
Wooki <hyunook02@naver.com>
2024-08-02 14:27:40 +0900

    boj 2475: 검증수

---
+++ VerificationNumber/Main.java
@@ -0,0 +1,16 @@
+package Backjoon.VerificationNumber;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x = 0;
+        while (in.hasNextInt()) {
+            int next = in.nextInt();
+            x += next * next;
+        }
+
+        System.out.println(x % 10);
+    }
+}
