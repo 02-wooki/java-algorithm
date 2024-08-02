6802fc40ce855ce9dfe7077a51dc022e8cc07b5c
Wooki <hyunook02@naver.com>
2024-08-02 17:47:44 +0900

    boj 15829: Hashing (50점)

---
+++ Hashing/Main.java
@@ -0,0 +1,24 @@
+package Backjoon.Hashing;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();   in.nextLine();
+
+        char[] c = in.nextLine().toCharArray();
+        int[] nums = new int[c.length];
+
+        for (int i = 0; i < c.length; i++)
+            nums[i] = c[i] - 'a' + 1;
+
+        long hashed = 0, powered = 1;
+        for (int i = 0; i < nums.length; i++) {
+            hashed += nums[i] * powered;
+            powered *= 31;
+        }
+
+        System.out.println(hashed % 1234567891);
+    }
+}
