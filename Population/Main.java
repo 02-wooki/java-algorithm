c3e0e49c17959c08b4be88e95fcec097d09f67cd
Wooki <hyunook02@naver.com>
2024-08-02 12:54:33 +0900

    boj 26561: Population

---
+++ Population/Main.java
@@ -0,0 +1,16 @@
+package Backjoon.Population;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int beg = in.nextInt(), time = in.nextInt();
+            System.out.println(beg - (time / 7) + (time / 4));
+        }
+    }
+}
