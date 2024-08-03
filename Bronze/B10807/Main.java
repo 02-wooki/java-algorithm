6eed16e4c555739d05c1b130e8a4201e70f0e848
Wooki <hyunook02@naver.com>
2024-08-03 17:38:35 +0900

    개수 세기

---
+++ Bronze/B10807/Main.java
@@ -0,0 +1,26 @@
+package Backjoon.Bronze.B10807;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int count = 0;
+        int v;
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = in.nextInt();
+
+        v = in.nextInt();
+
+        for (int i = 0; i < arr.length; i++)
+            if(arr[i] == v)
+                count++;
+
+        System.out.println(count);
+
+    }
+}
