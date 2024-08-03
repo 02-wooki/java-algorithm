1e4a9022d94b70b43b0700ef5847b003680d8ca6
Wooki <hyunook02@naver.com>
2024-08-03 18:41:01 +0900

    등록

---
+++ Bronze/B7287/Main.java
@@ -0,0 +1,8 @@
+package Backjoon.Bronze.B7287;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("93");
+        System.out.println("javahyunwook");
+    }
+}
