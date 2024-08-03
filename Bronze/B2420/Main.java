789f294c7815ded2aa9aa7aea0f21dc1b716dada
Wooki <hyunook02@naver.com>
2024-08-03 18:51:59 +0900

    사파리월드

---
+++ Bronze/B2420/Main.java
@@ -0,0 +1,10 @@
+package Backjoon.Bronze.B2420;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(Math.abs((long)in.nextInt() - (long)in.nextInt()));
+    }
+}
