825bdf11d84469a339ad2e7f4d21ed923ba54c73
wooki <hyunook02@naver.com>
2025-08-03 19:02:06 +0900

    2750: 수 정렬하기

---
+++ Bronze/B2750/Main.java
@@ -0,0 +1,23 @@
+package Backjoon.Bronze.B2750;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(in.readLine());
+
+        int[] list = new int[n];
+        while (n-- > 0)
+            list[n] = Integer.parseInt(in.readLine());
+
+        Arrays.sort(list);
+
+        for (int i: list)
+            System.out.println(i);
+    }
+}
